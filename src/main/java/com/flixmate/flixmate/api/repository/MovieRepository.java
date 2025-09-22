15318cf785357b5e302e4db9eab96ae3fa94283a
IT24200314 <it24200314.my.sliit.lk>
2025-09-22 10:44:49 +0530

    Replace remote files with my project

--- src/main/java/com/flixmate/flixmate/api/repository/MovieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.flixmate.flixmate.api.repository;
-
-import com.flixmate.flixmate.api.entity.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface MovieRepository extends JpaRepository<Movie, Integer> {
-    List<Movie> findByIsActiveTrue();
-}
