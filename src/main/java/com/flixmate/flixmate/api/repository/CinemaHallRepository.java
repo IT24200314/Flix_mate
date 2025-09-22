5d69abc57b98442c031c56b4e8f6e12c8265981f
IT24200314 <it24200314.my.sliit.lk>
2025-09-22 10:31:00 +0530

    Replace main branch files with my updated project files

--- src/main/java/com/flixmate/flixmate/api/repository/CinemaHallRepository.java
+++
@@ -1,7 +0,0 @@
-package com.flixmate.flixmate.api.repository;
-
-import com.flixmate.flixmate.api.entity.CinemaHall;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CinemaHallRepository extends JpaRepository<CinemaHall, Integer> {
-}
--- src/main/java/com/flixmate/flixmate/api/repository/MovieRepository.java
+++
@@ -1,7 +0,0 @@
-package com.flixmate.flixmate.api.repository;
-
-import com.flixmate.flixmate.api.entity.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRepository extends JpaRepository<Movie, Integer> {
-}
